94409a03efb2867e398a4cd385a286cbeec42160
HDavis1487 <HDavis1487@users.noreply.github.com>
2026-07-23 18:15:52 -0400

    tired
    dfsdkjfs

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container mx-auto p-8">
+    <h1 class="text-3xl font-bold underline mb-8">
+      Hello
+    </h1>
+    <button class="btn btn-primary flex  uppercase text-2xl text-red-500 items-center">
+      View map <Icon name="tabler:map" size="32" />
+    </button>
   </div>
 </template>
